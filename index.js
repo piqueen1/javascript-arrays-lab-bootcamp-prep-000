22c7481b8a839b56c9e3f54b24febab56e34f325
Finn Thye <piqueen@gmail.com>
2019-04-11 11:04:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  return kittens.concat('Broom');
+}
+
+function prependKitten(name) {
+  kittens.shift('Arnold');
+  return kittens;
+}
